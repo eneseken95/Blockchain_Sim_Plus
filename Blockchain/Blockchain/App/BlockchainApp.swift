8e61b24098177a882601ce7399a66b5a635dd330
Enes Eken <144843964+eneseken95@users.noreply.github.com>
2025-10-26 16:43:30 +0300

    Add SwiftUI Entry (BlockchainApp), Info.plist, App Icon, README, .gitignore

---
+++ Blockchain/Blockchain/App/BlockchainApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockchainApp.swift
+//  Blockchain
+//
+//  Created by Enes Eken 2 on 25.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BlockchainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Blockchain Sim+
+
+<img src="" alt="Logo" width="120" height="120" />
+
+### App Name: Blockchain Sim+
+This project is licensed under the Apache License 2.0. Copyright © 2025, Enes Eken.
